69c068e03bcd5be120c34356b525ac3d8fd57287
Jeffrey Wang <jeffwang16@gmail.com>
2015-03-20 13:16:27 -0500

    Adding precision to the ten-thousandths place
    Using printf and %0.4f to achieve rounding to the ten-thousandths place.

--- hardyweinberg.php
+++ hardyweinberg.php
@@ -27,12 +27,12 @@ if ( $_REQUEST['submit'] ) {
 	<hr />
 	<p><b>Note:</b> Unless otherwise listed, the below values are the frequencies of the respective values.</p>
 	<hr />
-	<p>p (dominant alleles) is <b><?php echo $p; ?></b>.</p>
-	<p>p&sup2; (homozygous dominant) is <b><?php echo $p2; ?></b>.</p>
-	<p>q (recessive alleles) is <b><?php echo $q; ?></b>.</p>
-	<p>q&sup2; (homozygous recessive) is <b><?php echo $q2; ?></b>.</p>
-	<p>2pq (heterozygous) is <b><?php echo $tpq; ?></b>.</p>
-	<p>Heterozygous and homozygous recessive combined is <b><?php echo $tpq + $q2; ?></b>.</p>
+	<p>p (dominant alleles) is <b><?php printf ( "%0.4f" , $p ); ?></b>.</p>
+	<p>p&sup2; (homozygous dominant) is <b><?php printf ( "%0.4f" , $p2 ); ?></b>.</p>
+	<p>q (recessive alleles) is <b><?php printf ( "%0.4f" , $q ); ?></b>.</p>
+	<p>q&sup2; (homozygous recessive) is <b><?php printf ( "%0.4f" , $q2 ); ?></b>.</p>
+	<p>2pq (heterozygous) is <b><?php printf ( "%0.4f" , $tpq ); ?></b>.</p>
+	<p>Heterozygous and homozygous recessive combined is <b><?php printf ( "%0.4f" , $tpq + $q2 ); ?></b>.</p>
 	<?php
 } else {
 	?>
